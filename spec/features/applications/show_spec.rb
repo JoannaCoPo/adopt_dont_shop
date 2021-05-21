15da27e9352124cfc3a42b4e3ca9a1ef0e3b2cde
JoannaCoPo <jococopo@gmail.com>
2021-05-21 16:55:15 -0600

    Fix typo

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'the application show' do
   it "shows an application and all its attributes" do
-    application = Application.create!(name: 'John Applicant', address: '1234 Turing Ave Denver, CO 81224', description: 'I have the time and space', desired_pets:'Scooby', status: 'status')
+    application = Application.create!(name: 'John Applicant', address: '1234 Turing Ave Denver, CO 81224', description: 'I have the time and space', desired_pets:'Scooby', status: 'Pending')
 
     visit "/applications/#{application.id}"
-
+    
     expect(page).to have_content(application.name)
     expect(page).to have_content(application.address)
     expect(page).to have_content(application.description)
