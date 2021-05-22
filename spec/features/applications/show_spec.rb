2b4277b3f426047fe95697ac622f6b32a81cc050
JoannaCoPo <jococopo@gmail.com>
2021-05-21 18:59:41 -0600

    Fix typo

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'the application show' do
     application = Application.create!(name: 'John Applicant', address: '1234 Turing Ave Denver, CO 81224', description: 'I have the time and space', desired_pets:'Scooby', status: 'Pending')
 
     visit "/applications/#{application.id}"
-    
+
     expect(page).to have_content(application.name)
     expect(page).to have_content(application.address)
     expect(page).to have_content(application.description)
