f9b06ea469653f4d926e579895a4ebb54114ec5a
JoannaCoPo <jococopo@gmail.com>
2021-05-23 11:33:07 -0600

    Remove user story

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -29,15 +29,5 @@ RSpec.describe 'the application new' do
 
     expect(current_path).to eq('/applications/new')
     expect(page).to have_content('Pet Adoption Application Form:')
-
   end
 end
-
-# Starting an Application, Form not Completed
-#
-# As a visitor
-# When I visit the new application page
-# And I fail to fill in any of the form fields
-# And I click submit
-# Then I am taken back to the new applications page
-# And I see a message that I must fill in those fields.
