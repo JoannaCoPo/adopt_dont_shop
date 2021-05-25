bc726269b56e3f6b1af07d616fd507867ad68539
JoannaCoPo <jococopo@gmail.com>
2021-05-25 17:04:54 -0600

    Change expectaion to have_link for subsequent functionality

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'the application show' do
-  xit "shows an application and all its attributes" do
+  it "shows an application and all its attributes" do
     application = Application.create!(name: 'Winston Bishop', address: '1234 Turing Ave', description: 'I have the time and space', desired_pets:'Ferguson', status: 'Pending', city: 'Portland', state: 'OR', zip: 92377)
     visit "/applications/#{application.id}"
 
     expect(page).to have_content(application.name)
     expect(page).to have_content(application.address)
     expect(page).to have_content(application.description)
-    expect(page).to have_content(application.desired_pets)
     expect(page).to have_content(application.status)
     expect(page).to have_content(application.city)
     expect(page).to have_content(application.state)
@@ -16,7 +15,7 @@ RSpec.describe 'the application show' do
   end
 
   # WILL NEED TO UTILIZE PARTIALS AT SOME POINT
-  xit 'displays section to search for a pet to add to application' do
+  it 'displays section to search for a pet to add to application' do
     application = Application.create!(name: 'Winston Bishop', address: '1234 Turing Ave', description: 'I have the time and space', desired_pets:'Ferguson', status: 'Pending', city: 'Portland', state: 'OR', zip: 92377)
     visit "/applications/#{application.id}"
 
@@ -31,18 +30,18 @@ RSpec.describe 'the application show' do
     visit "/applications/#{application.id}"
 
     click_button('Search')
-    expect(current_path).to eq("/applications/:id")
+    expect(current_path).to eq("/applications/#{application.id}")
+  end
+
+  it 'displays under the search bar any Pet whose name matches the input search' do
+    shelter = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: shelter.id)
+    application = Application.create!(name: 'Winston Bishop', address: '1234 Turing Ave', description: 'I have the time and space', desired_pets:'Lobster', status: 'Pending', city: 'Portland', state: 'OR', zip: 92377)
+    visit "/applications/#{application.id}"
+
+    fill_in(:search, with: 'Lobster')
+    click_button('Search')
+    expect(page).to have_link('Lobster')
   end
 end
-
-# Searching for Pets for an Application
-#
-# As a visitor
-# When I visit an application's show page
-# And that application has not been submitted,
-# Then I see a section on the page to "Add a Pet to this Application"
-# In that section I see an input where I can search for Pets by name
-# When I fill in this field with a Pet's name
-# And I click submit,
-# Then I am taken back to the application show page
-# And under the search bar I see any Pet whose name matches my search
